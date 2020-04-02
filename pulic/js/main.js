2becfeae10fae1b7d12b3b21dbfebb9d4db9bb80
Kachi123 <kachi.erugo@codex.com.ng>
2020-04-02 03:20:43 +0100

    adding files

---
+++ pulic/js/main.js
@@ -0,0 +1,42 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages')
+
+const socket = io();
+
+// Message from server
+socket.on('message', message => {
+    console.log(message);
+    outputMessage(message);
+
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+
+// Message Submit
+chatForm.addEventListener('submit', (e) => {
+ e.preventDefault();
+
+ // Get message text
+ const msg = e.target.elements.msg.value;
+
+ // Emit message to server
+ socket.emit('chatMessage', msg);
+
+ // Clear input
+ e.target.elements.msg.value = '';
+ e.target.elements.msg.focus();
+});
+
+
+// output Message to DOM
+function  outputMessage(message) {
+ const div = document.createElement('div');
+ div.classList.add('message');
+ div.innerHTML = `<p class="meta">Mary <span>9:15pm</span></p>
+ <p class="text">
+    ${message}
+ </p>`;
+ document.querySelector('.chat-messages').appendChild(div);
+}
---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+// Set static folder
+app.use(express.static(path.join(__dirname, '/pulic')));
+
+
+const botName = 'Konnect Bot';
+
+// Run when client connects
+io.on('connection', socket => {
+    // Welcome current user
+  socket.emit('message', formatMessage(botName, 'Welcome to Konnect!, Catch up with your teammates '));
+
+  // Broadcast when a user connects
+  socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+      io.emit('message', formatMessage(botName, 'A user has left the chat'));
+  });
+
+  // listen for chatMessage
+socket.on('chatMessage', msg => {
+    io.emit('message', formatMessage('USER', msg));
+   });
+});
+
+
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
